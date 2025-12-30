57737c701032dc9dca1ef3af91c1da27b48213f4
Curtis Bezencon <cbezenco@c2r4s4.42lausanne.ch>
2025-12-30 14:35:59 +0100

    fix some builtins

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -20,7 +20,7 @@ char	*ft_strjoin(const char *s1, const char *s2)
 
 	i = 0;
 	j = 0;
-	if (!s1 || !s2)
+	if (!s1 && !s2)
 		return (0);
 	str = malloc(sizeof(char)
 			* (ft_strlen(s1) + ft_strlen(s2) + 1));
